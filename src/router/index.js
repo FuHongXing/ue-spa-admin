3c6737325c6409f20e920f73fb2004caa0d3b6af
Aaron-github <jahol@vip.qq.com>
2018-08-23 10:33:00 +0800

    add errorLog

--- src/router/index.js
+++ src/router/index.js
@@ -141,6 +141,21 @@ export const asyncRouterMap = [{
     }
   }]
 },
+{
+  path: '/error-log',
+  component: Layout,
+  redirect: 'noredirect',
+  children: [{
+    path: 'log',
+    component: () =>
+        import('@/views/errorLog/index'),
+    name: 'ErrorLog',
+    meta: {
+      title: 'errorLog',
+      icon: 'bug'
+    }
+  }]
+},
 nestedRouter,
 {
   path: '/404',
